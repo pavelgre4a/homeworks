8668e3267c647cb2c8dac27f8ddab8cae6c2b378
pavelgre4a <103334312+pavelgre4a@users.noreply.github.com>
2022-04-21 22:10:06 +0300

    Add files via upload

---
+++ QA/HW2-1.js
@@ -0,0 +1,58 @@
+// // #1-3
+// let item_1 = 5;
+
+// console.log(item_1);
+
+// // #4-6
+// let item_2 = 3;
+
+// console.log(item_2);
+
+// // #7-9
+// let item_3 = item_1 + item_2;
+
+// console.log(item_3);
+
+// // #10-14
+// let item_4 = 'Yolochka';
+
+// console.log(item_4);
+
+// console.log(item_3 + item_4);
+
+// console.log(item_3 * item_4);
+
+// // #15-25
+// let item_5 = item_3;
+// let item_6;
+// let item_6_type;
+
+//     item_6 = 15;
+//     item_6_type = typeof item_6;
+
+//     console.log("item_6 == ", item_6 + " item_6_type ==", item_6_type);
+
+//     let item_7 = item_6 = String(item_6);
+
+//     let item_7_type = typeof item_7;
+
+//     console.log("item_7 == " + item_7 + " item_7_type ==" + item_7_type);
+
+// #26-33
+    let age_1 = 10;
+    let age_2 = 18;
+    let age_3 = 60;
+
+    if (age_1 < age_2) {
+        console.log("You don't have acces cause your age is", age_1, "It's less then", age_2)
+    } 
+    else if (age_1 >= age_2 && age_1 < age_3) {
+        console.log("Welcome!")
+    }
+    else if (age_1 > age_3) {
+        console.log("Keep calm and look Culture channel")
+    }
+    else {
+        console.log("Technical work")
+    }
+}
