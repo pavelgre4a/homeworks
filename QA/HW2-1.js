0e7c80b1295c40a19a210187d216cb44e456a84d
pavelgre4a <103334312+pavelgre4a@users.noreply.github.com>
2022-04-21 22:11:08 +0300

    Update HW2-1.js

--- QA/HW2-1.js
+++ QA/HW2-1.js
@@ -1,42 +1,42 @@
-// // #1-3
-// let item_1 = 5;
+// #1-3
+let item_1 = 5;
 
-// console.log(item_1);
+console.log(item_1);
 
-// // #4-6
-// let item_2 = 3;
+// #4-6
+let item_2 = 3;
 
-// console.log(item_2);
+console.log(item_2);
 
-// // #7-9
-// let item_3 = item_1 + item_2;
+// #7-9
+let item_3 = item_1 + item_2;
 
-// console.log(item_3);
+console.log(item_3);
 
-// // #10-14
-// let item_4 = 'Yolochka';
+// #10-14
+let item_4 = 'Yolochka';
 
-// console.log(item_4);
+console.log(item_4);
 
-// console.log(item_3 + item_4);
+console.log(item_3 + item_4);
 
-// console.log(item_3 * item_4);
+console.log(item_3 * item_4);
 
-// // #15-25
-// let item_5 = item_3;
-// let item_6;
-// let item_6_type;
+// #15-25
+let item_5 = item_3;
+let item_6;
+let item_6_type;
 
-//     item_6 = 15;
-//     item_6_type = typeof item_6;
+    item_6 = 15;
+    item_6_type = typeof item_6;
 
-//     console.log("item_6 == ", item_6 + " item_6_type ==", item_6_type);
+    console.log("item_6 == ", item_6 + " item_6_type ==", item_6_type);
 
-//     let item_7 = item_6 = String(item_6);
+    let item_7 = item_6 = String(item_6);
 
-//     let item_7_type = typeof item_7;
+    let item_7_type = typeof item_7;
 
-//     console.log("item_7 == " + item_7 + " item_7_type ==" + item_7_type);
+    console.log("item_7 == " + item_7 + " item_7_type ==" + item_7_type);
 
 // #26-33
     let age_1 = 10;
